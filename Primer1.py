5d06975f079036b3e565f8622f2ab37b6e8e6461
Vladislav <adrenalin_1231@mail.ru>
2020-11-24 14:31:10 +0300

    add all files

---
+++ Primer1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+if __name__ == '__main__':
+    # Ввести список одной строкой.
+    A = list(map(int, input().split()))
+    # Проверить количество элементов списка.
+    if len(A) != 10:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
+    # Найти искомую сумму.
+    s = 0
+    for item in A:
+        if abs(item) < 5:
+            s += item
+    print(s)
